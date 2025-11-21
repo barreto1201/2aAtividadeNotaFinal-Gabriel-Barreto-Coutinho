d1f68d5bae209bd7c4561857ca3052e9544d1310
Gabriel Barreto <gabrielbarretocoutinho@gmail.com>
2025-11-20 23:15:31 -0300

    Create add_tarefa.php

---
+++ QUESTÃO-2/add_tarefa.php
@@ -0,0 +1,16 @@
+<?php
+require 'database.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $descricao = $_POST['descricao'] ?? '';
+    $vencimento = $_POST['vencimento'] ?? '';
+
+    if ($descricao && $vencimento) {
+        $stmt = $db->prepare("INSERT INTO tarefas (descricao, vencimento) VALUES (?, ?)");
+        $stmt->execute([$descricao, $vencimento]);
+    }
+}
+
+header("Location: index.php");
+exit;
+?>
