85964490d6fd9bf2f4fbc1f7374a5733203e2ee8
Gabriel Barreto <gabrielbarretocoutinho@gmail.com>
2025-11-20 23:05:50 -0300

    Create database.php

---
+++ QUESTÃO-1/database.php
@@ -0,0 +1,19 @@
+<?php
+
+try {
+    $db = new PDO("sqlite:livraria.db");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $db->exec("CREATE TABLE IF NOT EXISTS livros (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            titulo TEXT NOT NULL,
+            autor TEXT NOT NULL,
+            ano INTEGER NOT NULL
+        )"
+    );
+    
+} catch (PDOException $e) {
+    echo "Erro ao conectar: " . $e->getMessage();
+    exit;
+}
+?>
